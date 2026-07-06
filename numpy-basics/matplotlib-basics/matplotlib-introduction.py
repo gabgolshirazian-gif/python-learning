ce18e53fd00f5b551b9d7975b6eb25c965b811c8
gabgolshirazian-gif <gabgolshirazian@gmail.com>
2026-07-06 21:38:08 +0330

    matplotlib-sin&cos.png

--- numpy-basics/matplotlib-basics/matplotlib-introduction.py
+++ numpy-basics/matplotlib-basics/matplotlib-introduction.py
@@ -9,3 +9,10 @@ y = x**2
 plt.plot(x,y)
 plt.savefig("matplotlib-introduction.png")
 plt.show()
+
+
+x = np.linspace(0,4*np.pi,100)
+plt.plot(x,np.sin(x))
+plt.plot(x,np.cos(x))
+plt.savefig("matplotlib-sin&cos.png")
+plt.show()
