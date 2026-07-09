45ad014ba28c699ec9d674f1d9f2dde0330e3ee9
gabgolshirazian-gif <gabgolshirazian@gmail.com>
2026-07-09 15:13:10 +0330

    matplotlib-introduction.png
    Plotting x=y^2 and Plotting sin and cos plots using the matplotlib module.

---
+++ matplotlib-basics/matplotlib-introduction.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-10,11,1)
+y = x**2
+
+
+plt.plot(x,y)
+plt.savefig("matplotlib-introduction.png")
+plt.show()
+
+
+x = np.linspace(0,4*np.pi,100)
+plt.plot(x,np.sin(x),label="sin",color= "red",linewidth = 3,linestyle = "--")
+plt.plot(x,np.cos(x),label="cos",color="green",linewidth = 3,linestyle = "-.")
+plt.savefig("matplotlib-sin&cos.png")
+plt.title("sin&cos")
+plt.xlabel("x")
+plt.ylabel("x=sin , x=cos")
+plt.grid(axis="both")
+plt.legend()
+plt.show()
