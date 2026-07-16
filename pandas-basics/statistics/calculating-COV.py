2e350537f28d8e3fffaf424d23a9ac730b9c968d
gabgolshirazian-gif <gabgolshirazian@gmail.com>
2026-07-16 22:04:50 +0330

    calculating-COV.py
    The usage of COV (coefficient of variation) to STD is as follows:
    When we have two statistical populations, for example, the scores of students in a classroom in Japan and the scores of students in a classroom in Iran, considering that student scores in Iran are considered between zero and twenty and in Japan they are considered between zero and one hundred, calculating STD is not correct for comparing these two statistical populations. Therefore, when our statistical population uses different units of measurement or different criteria, using COV, we can eliminate this measurement criterion and compare the two statistical populations more accurately.

---
+++ pandas-basics/statistics/calculating-COV.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#the scores of students in Japan
+a = np.random.randint(0,100,size=50)
+#the scores of students in Iran
+b = np.random.randint(0,20,size=60)
+
+
+std_a = a.std()
+mean_a = a.mean()
+
+std_b = b.std()
+mean_b = b.mean()
+
+
+#calculating COV
+a_cov = std_a / mean_a
+b_cov = std_b / mean_b
+
+
+print(a_cov)
+
+print("-------------------------------")
+
+print(b_cov)
