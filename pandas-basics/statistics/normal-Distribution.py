88bffda9f4fd4e4f532a730e50d9d48d1df837a3
gabgolshirazian-gif <gabgolshirazian@gmail.com>
2026-07-19 19:20:01 +0330

    Update normal-Distribution.py

--- pandas-basics/statistics/normal-Distribution.py
+++ pandas-basics/statistics/normal-Distribution.py
@@ -11,16 +11,20 @@ plt.show()
 mean = arr.mean()
 std = arr.std()
 
+
 up = mean + std
 down = mean - std
+
 
 up1 = mean + (2*std)
 down1 = mean - (2*std)
 
+#arr1 is (mean + 2STD) and (mean - 2STD)
 #the result is 68 % 
 arr1 = arr[(arr > down) & (arr < up)]
 print(((arr1.size) / 100000) * 100)
 
+#arr2 is (mean + STD) and (mean - STD)
 #the result is 95 %
 arr2 = arr[(arr > down1) & (arr < up1)]
 print(((arr2.size) / 100000) * 100)
