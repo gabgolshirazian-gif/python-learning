a48cd8d61dc03f8dc30cba4b02286abd113cf0f1
gabgolshirazian-gif <gabgolshirazian@gmail.com>
2026-07-20 14:05:32 +0330

    skewness&kurt-Consepts.py
    In the concept of skewness, the more the tendency, concentration, and stretch of the data are to the right, then we say negative skewness, and vice versa, if the tendency, concentration, and stretch of the data are to the right, then we say positive skewness.
    In positive skewness, the values ​​of mode, median, and mean are as follows: mode <median <mean
    And in negative skewness, the values ​​of mode, median, and mean are as follows: mode >median >mean
    
    Usually, if the skewness is smaller than -1 and larger than +1, the skewness is usually high.
    But if the skewness is between +0.5 and -0.5, the distribution is normal.
    
    In the concept of kurtosis, the more the data is stretched upwards or is longer, it is called positive kurt, meaning that our data is more concentrated in the center
    And the more the data is stretched downwards or is shorter, it is called negative kurt, meaning that our data is scattered.

---
+++ pandas-basics/statistics/skewness&kurt-Consepts.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+import seaborn as sns
+
+
+data = pd.read_csv("student_data.csv")
+
+print(data.head())
+
+
+print("-------------------------------")
+
+
+grade = data["G1"] + data["G2"] + data["G3"]
+grade.hist(bins=50)
+plt.show()
+
+
+#Calculating skew (in this case skew is -0.14)
+print(grade.skew())
+
+#Calculating kurt (in this case kurt is -0.33)
+print(grade.kurt())
+
+sns.histplot(grade ,bins=50 , kde=True)
+plt.show()
+
+
+mean = grade.mean()
+std = grade.std()
+
+
+# If, in this example, we consider an incentive to increase students' scores and assume that
+# students with a GPA below 43 have had an increase in GPA by STD/2, then the skewness and kurt
+# situation is as follows:
+
+grade[grade < 43] = grade[grade < 43] + (std/2) 
+
+
+# As can be seen in the second plot, the skewness has shifted to the right, meaning that the 
+#average score of students below 43 has increased, and so has kurt.
+sns.histplot(grade ,bins=50 , kde=True)
+plt.show()
