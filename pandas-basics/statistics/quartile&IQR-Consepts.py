93e322f2cf65190c39ff27224de841eac785598c
gabgolshirazian-gif <gabgolshirazian@gmail.com>
2026-07-20 18:49:54 +0330

    quartile&IQR-Consepts.py
    In the quartile discussion, our data is divided into four parts, and each part contains 25% of the data.
    That is, if, for example, in an array or a set, the number of our data is 10, for example {2,4,5,7,8,12,16,18,21,25} in this set, the first quartile or Q1 is equal to 2.75, Q2 is equal to 5.5, Q3 is equal to 8.25, and Q2 is equal to the median.
    
    One of the applications that can be expressed for quartile is IQR or interquartile
    range, which is expressed as follows:
    If Q3 - Q2 > Q2 - Q1, then the skewness is positive.
    and if Q3 - Q2 < Q2 - Q1, then the skewness is negative.

---
+++ pandas-basics/statistics/quartile&IQR-Consepts.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+import seaborn as sns
+
+
+data = pd.read_csv("student_data.csv")
+
+grade = data["G1"] + data["G2"] + data["G3"]
+
+
+# Q1 = 25 , Q2 = 32 , Q3 = 40
+print(grade.describe())
+
+data["ave"] = grade / 3 
+
+# In the graph drawn for the columns ave, G1, G2, and G3, there is a rectangle
+# for each box plot. The upper side is the value of Q3, the lower side is Q1, and
+# the middle green line is Q2. For each of the columns G1, G2, and G3 and the
+# value of ave, the degree of skewness and positive or negative skewness can be
+# examined intuitively.
+data.boxplot(["ave" , "G1" , "G2" , "G3"])
+plt.show()
